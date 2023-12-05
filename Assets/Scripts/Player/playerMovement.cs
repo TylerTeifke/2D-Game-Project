030c51d87d7ed281dc6038a1a1bc3c2a2298ee1f
TylerTeifke <tylerteifke@gmail.com>
2023-12-05 12:12:20 -0800

    Tutorials added
    Added tutorials that appear when the player collects a power up that explains how to use the power up

--- Assets/Scripts/Player/playerMovement.cs
+++ Assets/Scripts/Player/playerMovement.cs
@@ -15,6 +15,11 @@ public class playerMovement : MonoBehaviour
     // UI object to display winning text.
     public GameObject winTextObject;
 
+    //UI objects for displaying tutorials
+    public GameObject dashTutorial;
+    public GameObject upwardTutorial;
+    public GameObject downwardTutorial;
+
     public float speed = 40f;
     float horizontalMovement = 0f;
     bool jump = false;
@@ -27,11 +32,18 @@ public class playerMovement : MonoBehaviour
     bool canUpward = false;
     bool canDownward = false;
 
+    //Will tell if a tutorial is active
+    bool tutorialActive = false;
+
     // Start is called before the first frame update
     void Start()
     {
         // Initially set the win text to be inactive.
         winTextObject.SetActive(false);
+
+        dashTutorial.SetActive(false);
+        upwardTutorial.SetActive(false);
+        downwardTutorial.SetActive(false);
     }
 
     // Update is called once per frame
@@ -57,6 +69,14 @@ public class playerMovement : MonoBehaviour
         if(Input.GetButtonDown("Downward") && canDownward){
             downwardSlash = true;
         }
+
+        if (Input.GetKeyDown(KeyCode.T) && tutorialActive){
+            dashTutorial.SetActive(false);
+            upwardTutorial.SetActive(false);
+            downwardTutorial.SetActive(false);
+            tutorialActive = false;
+            Time.timeScale = 1f;
+        }
     }
 
     public void OnLanding ()
@@ -75,16 +95,26 @@ public class playerMovement : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other){
         //When a specific pick up has been collected, the power up tied to that pick up will be activated
+        //When a power up is collected, a tutorial will appear on screen that tells the player how to use it
         if(other.CompareTag("Dash")){
             canDash = true;
+            Time.timeScale = 0f;
+            tutorialActive = true;
+            dashTutorial.SetActive(true);
         }
 
         if(other.CompareTag("Upward")){
             canUpward = true;
+            Time.timeScale = 0f;
+            tutorialActive = true;
+            upwardTutorial.SetActive(true);
         }
 
         if(other.CompareTag("Downward")){
             canDownward = true;
+            Time.timeScale = 0f;
+            tutorialActive = true;
+            downwardTutorial.SetActive(true);
         }
 
         //Will end the game if all three collectables have been collected
